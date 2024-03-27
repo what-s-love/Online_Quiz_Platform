4d2b036261e677aeaa87ab096dd88688d7b9a9ea
Nursultan Beishekeev <nursultanzynwa@gmail.com>
2024-03-27 21:06:00 +0600

    Added libraries to pom.xml, added migrations: 1) Created required tables 2) Inserted some data

--- src/main/java/kg/attractor/online_quiz_platform/OnlineQuizPlatformApplication.java
+++ src/main/java/kg/attractor/online_quiz_platform/OnlineQuizPlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineQuizPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineQuizPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineQuizPlatformApplication.class, args);
+    }
 
 }
