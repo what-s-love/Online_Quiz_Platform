bf4c9968c03e42d102c4926681db61acb80f55fd
Nursultan Beishekeev <nursultanzynwa@gmail.com>
2024-03-27 18:57:47 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/online_quiz_platform/OnlineQuizPlatformApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.online_quiz_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/attractor/online_quiz_platform/OnlineQuizPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.online_quiz_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
