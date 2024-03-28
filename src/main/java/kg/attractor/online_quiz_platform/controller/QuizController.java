b485faa6593e58d95dace12a328269ab1bb81e1e
Vladislav K <barabanabanan@gmail.com>
2024-03-28 21:35:11 +0600

    Added QuizController.createQuiz()

--- src/main/java/kg/attractor/online_quiz_platform/controller/QuizController.java
+++ src/main/java/kg/attractor/online_quiz_platform/controller/QuizController.java
@@ -1,12 +1,26 @@
 package kg.attractor.online_quiz_platform.controller;
 
+import jakarta.validation.Valid;
+import kg.attractor.online_quiz_platform.dto.QuizDto;
+import kg.attractor.online_quiz_platform.dto.UserDto;
+import kg.attractor.online_quiz_platform.service.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("quizzes")
 @RequiredArgsConstructor
 public class QuizController {
+    private final QuizService quizService;
 
+    @PostMapping
+    public HttpStatus createQuiz(@RequestBody @Valid QuizDto quizDto, Authentication auth) {
+        quizService.createQuizAndReturnId(quizDto, auth);
+        return HttpStatus.OK;
+    }
 }
