777d2fe20b107d35c6750b4561e6739829e5dc24
Vladislav K <barabanabanan@gmail.com>
2024-03-28 19:48:31 +0600

    added UserService.class and createUserAndReurnId()

--- src/main/java/kg/attractor/online_quiz_platform/service/UserService.java
+++ src/main/java/kg/attractor/online_quiz_platform/service/UserService.java
@@ -1,4 +1,18 @@
 package kg.attractor.online_quiz_platform.service;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserDao userDao;
+    
+    public int createUserAndReturnId(UserDto userDto) {
+        User user = dtoToModel(userDto);
+        return userDao.createUserAndReturnId(user);
+    }
 }
