0f5d1e3cb4ee14427c0529baf8c76d659e96b198
Vladislav K <barabanabanan@gmail.com>
2024-03-29 16:40:26 +0600

    Added model Option

--- src/main/java/kg/attractor/online_quiz_platform/dao/OptionDao.java
+++ src/main/java/kg/attractor/online_quiz_platform/dao/OptionDao.java
@@ -1,11 +1,11 @@
 package kg.attractor.online_quiz_platform.dao;
 
-import kg.attractor.online_quiz_platform.model.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 
 
@@ -14,10 +14,10 @@ import java.util.List;
 public class OptionDao {
     private final JdbcTemplate template;
 
-    public List<Question> getOptionsByQuestionId(int questionId) {
+    public List<Option> getOptionsByQuestionId(int questionId) {
         String sql = """
             select * from questions where quizId = ?;
             """;
-        return template.query(sql, new BeanPropertyRowMapper<>(Question.class), questionId);
+        return template.query(sql, new BeanPropertyRowMapper<>(Option.class), questionId);
     }
 }
---
+++ src/main/java/kg/attractor/online_quiz_platform/model/Option.java
@@ -0,0 +1,8 @@
+package kg.attractor.online_quiz_platform.model;
+
+public class Option {
+    private int id;
+    private int questionId;
+    private String optionText;
+    private Boolean isCorrect;
+}
